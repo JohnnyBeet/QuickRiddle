ddd5463b58583b4c94a21af0b78e3dc163e91d38
WoobieApps <piotrasel@gmail.com>
2023-05-06 22:12:52 +0200

    ADD save type and difficulty
    Saving type of riddle and its difficulty in global variable in app.py

--- project/run.py
+++ project/run.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
+import json
 app = Flask(__name__)
+
+difficulty = ''
+type = ''
 
 @app.route("/")
 @app.route("/home")
@@ -25,6 +29,21 @@ def signup():
 def difficulty():
     return render_template('difficulty.html')
 
+@app.route("/riddle")
+def riddle():
+    return render_template('riddle.html')
+
+@app.route("/type_post", methods=['POST'])
+def type_post():
+    type = json.loads(request.get_json())['type']
+    app.logger.info(f"Chosen type: {type}")
+    return type
+
+@app.route("/difficulty_post", methods=['POST'])
+def difficulty_post():
+    difficulty = json.loads(request.get_json())['difficulty']
+    app.logger.info(f"Chosen difficulty: {difficulty}")
+    return difficulty
 
 
 if __name__ == '__main__':
