07e1f12b69610c835eb96d63409c58e95182d4cd
justynagibas <juztynagibas@gmail.com>
2023-06-22 15:14:07 +0200

    Database structure changes.

--- project/database/create_database.py
+++ project/database/create_database.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 from .difficulty_level import create_difficulty_tab
-from .riddles import create_math_riddles_tab, create_word_riddles_tab
+from .riddles import create_riddles_tab
 from .users import create_users_table
+from .category import create_category_tab
+from .completed_riddles import create_completed_riddles_tab
 
 from project import app
 
@@ -17,35 +19,51 @@ def insert_difficulty_level(db, Difficulty):
     db.session.commit()
 
 
-def insert_math_riddles(db):
+def insert_categories(db, Category):
+    word_riddle = Category(category='word_riddle')
+    math_riddle = Category(category='math_riddle')
+    db.session.add(word_riddle)
+    db.session.add(math_riddle)
+    db.session.commit()
+
+
+def insert_math_riddles(db, Riddles):
     math_data = pd.read_csv("database/zadania_kangur.csv", sep=";")
     math_data["Question"] = math_data["Question"] + ' ' + math_data["A"] + ' ' + math_data["B"] + ' ' + math_data[
         "C"] + ' ' + math_data["D"] + ' ' + math_data["E"]
     math_data = math_data.drop(columns=["A", "B", "C", "D", "E"])
     math_data.rename(columns={"Question": "content", "Answer": "answer", "Level": "level"}, inplace=True)
-    math_data.index += 1
-    math_data.index.names = ['id']
-    math_data.to_sql("math_riddles", con=db.engine, if_exists="append")
+    math_data["category"] = 2
+    # math_data.to_sql("riddles", con=db.engine, if_exists="append", index=False)
+    for index, data in math_data.iterrows():
+        row = Riddles(content=data["content"], answer=data['answer'], level=data['level'], category=data['category'])
+        db.session.add(row)
+    db.session.commit()
 
 
-def insert_word_riddles(db):
+def insert_word_riddles(db, Riddles):
     word_data = pd.read_csv("database/word_riddles.csv", sep=";")
     word_data.rename(columns={"QUESTIONS": "content", "ANSWERS": "answer", "Difficulty": "level"}, inplace=True)
-    word_data.index += 1
-    word_data.index.names = ['id']
-    word_data.to_sql("word_riddles", con=db.engine, if_exists="append")
+    word_data['category'] = 1
+    # word_data.to_sql("riddles", con=db.engine, if_exists="append", index=False)
+    for index, data in word_data.iterrows():
+        row = Riddles(content=data["content"], answer=data['answer'], level=data['level'], category=data['category'])
+        db.session.add(row)
+    db.session.commit()
 
 
 def create_tables(db):
     Difficulty = create_difficulty_tab(db)
-    MathRiddles = create_math_riddles_tab(db)
-    WordRiddles = create_word_riddles_tab(db)
+    Category = create_category_tab(db)
+    Riddles = create_riddles_tab(db)
     Users = create_users_table(db)
+    CompletedRiddles = create_completed_riddles_tab(db)
     with app.app_context():
         db.create_all()
         if db.session.query(Difficulty).first() is None:
             insert_difficulty_level(db, Difficulty)
-        if db.session.query(MathRiddles).first() is None:
-            insert_math_riddles(db)
-        if db.session.query(WordRiddles).first() is None:
-            insert_word_riddles(db)
+        if db.session.query(Category).first() is None:
+            insert_categories(db, Category)
+        if db.session.query(Riddles).first() is None:
+            insert_math_riddles(db, Riddles)
+            insert_word_riddles(db, Riddles)
--- project/database/users.py
+++ project/database/users.py
@@ -8,8 +8,6 @@ def create_users_table(db):
         id = Column(Integer, Sequence("seq_users_id", start=1), primary_key=True)
         user_name = Column(String, unique=True, nullable=False)
         password = Column(String, nullable=False)
-        math_riddles = Column(ARRAY(Integer), nullable=True, default=[])
-        word_riddles = Column(ARRAY(Integer), nullable=True, default=[])
         __table_args__ = (
             CheckConstraint("LENGTH(user_name) > 3"),
             CheckConstraint("LENGTH(password) = 25")
