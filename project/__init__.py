04a9de983f1b19a11375b38338931e1ba56184b7
Jan Pomykacz <jasiek2miliony@gmail.com>
2023-06-27 21:11:34 +0200

    Added automatic database generation

--- project/__init__.py
+++ project/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from sqlalchemy_utils import database_exists, create_database
 
 db_url = "postgresql://postgres:postgres@localhost:5432/Riddles"
+
+if not database_exists(db_url):
+    create_database(db_url)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
